c42d8692c29b58b76adf806440b6ee3830f242fb
Ixxith <60019877+Ixxith@users.noreply.github.com>
2021-04-07 12:06:28 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BYU_FEG.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
