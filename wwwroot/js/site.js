b244a563185feec2b19865ad97d845892e364217
Briton Demars <bdemars@byu.edu>
2021-04-08 20:18:09 -0600

    AddRecord Form basic layout complete. Need to brainstorm and finalize form entry with database setup

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,56 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+//Code for Artifact Found Dynamic Input Hidden Fields
+function yesnoCheck() {
+    if (document.getElementById('artifactFoundY').checked) {
+        document.getElementById('ifYes').style.display = 'block';
+    } else {
+        document.getElementById('ifYes').style.display = 'none';
+    }
+}
+
+//Code for Hair Taken Dynamic Input Hidden Fields
+function yesnoCheck2() {
+    if (document.getElementById('hairTakenY').checked) {
+        document.getElementById('ifYes2').style.display = 'grid';
+    } else {
+        document.getElementById('ifYes2').style.display = 'none';
+    }
+}
+
+//Code for Soft Tissue Taken Dynamic Input Hidden Fields
+function yesnoCheck3() {
+    if (document.getElementById('softTissueTakenY').checked) {
+        document.getElementById('ifYes3').style.display = 'grid';
+    } else {
+        document.getElementById('ifYes3').style.display = 'none';
+    }
+}
+
+//Code for Bone Taken Dynamic Input Hidden Fields
+function yesnoCheck4() {
+    if (document.getElementById('boneTakenY').checked) {
+        document.getElementById('ifYes4').style.display = 'grid';
+    } else {
+        document.getElementById('ifYes4').style.display = 'none';
+    }
+}
+
+//Code for Tooth Taken Dynamic Input Hidden Fields
+function yesnoCheck5() {
+    if (document.getElementById('toothTakenY').checked) {
+        document.getElementById('ifYes5').style.display = 'grid';
+    } else {
+        document.getElementById('ifYes5').style.display = 'none';
+    }
+}
+
+//Code for Textile Taken Dynamic Input Hidden Fields
+function yesnoCheck6() {
+    if (document.getElementById('textileTakenY').checked) {
+        document.getElementById('ifYes6').style.display = 'grid';
+    } else {
+        document.getElementById('ifYes6').style.display = 'none';
+    }
+}
+
