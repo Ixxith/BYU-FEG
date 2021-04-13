b93bb76b473b0d2292c51bad1d984eb17d316ecd
Emma Haynes <emmaehaynes@gmail.com>
2021-04-13 13:22:03 -0600

    Details page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -140,8 +140,31 @@ namespace BYU_FEG.Controllers
         [HttpPost] //details view
         public IActionResult ByufegDetails(int ByufegId)
         {
-            Byufeg byufeg = context.Byufeg.Find(ByufegId);
-            return View("Details", byufeg);
+            //FIRST TRY
+            //Byufeg byufeg = context.Byufeg.Find(ByufegId);
+            //return View("Details", byufeg);
+
+            //SECOND TRY
+            //var Byufeg = new Byufeg() { };
+            //var Burial = new Burial() { };
+            //var Attachment = new Attachment() { };
+            //var DetailsViewModel = new DetailsViewModel
+            //{
+            //    byufeg = Byufeg,
+            //    burial = Burial,
+            //    attachment = Attachment
+            //};
+
+            //return View("Details", DetailsViewModel);
+
+            //THIRD TRY
+            var byufeg = context.Byufeg.FirstOrDefault(x => x.ByufegId == ByufegId);
+            return View(new DetailsViewModel
+            {
+                byufeg = byufeg,
+                attachment = context.Attachment.Where(x => x.ByufegId == ByufegId),
+                burial = context.Burial.FirstOrDefault(x => x.BurialId == byufeg.BurialId)
+            });
         }
 
         [HttpPost]
---
+++ Models/ViewModel/DetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BYU_FEG.Models.ViewModel
+{
+    public class DetailsViewModel
+    {
+        public Byufeg byufeg { get; set; }
+        public Burial burial { get; set; }
+        public IEnumerable<Attachment> attachment { get; set; }
+    }
+}
